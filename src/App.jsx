f2d281dcb1fa1d74702cf3c8c33ead94a6f9ee17
Farrukh Babar <farrukhbabar.devv@gmail.com>
2025-12-15 11:50:28 +0500

    intial-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const App = () => {
+  return (
+    <div >
+      <h1 className="bg-yellow-600 text-red-500">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default App;
